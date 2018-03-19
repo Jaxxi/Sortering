fa7fd1c004ae0722514771d211aa9ae2612a5615
Jaxxi <te14.linus.olofsson@umea.nti.se>
2018-03-19 08:51:15 +0100

    Initial commit

---
+++ Sort.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Linus&Elias on 2018-03-12.
+ */
+public class Sort {
+    public static void selSort(int[] numbers) {
+
+    }
+
+    public static void selSort(double[] numbers) {
+
+    }
+
+    public static void selSortStringLength(String[] words) {
+
+    }
+
+    public static void selSortStringAlpha(String[] words) {
+
+    }
+}
